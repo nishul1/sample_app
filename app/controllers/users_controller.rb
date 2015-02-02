7d0eda522fa747891cebb050d343c99e1b90fba9
Nishul Saperia <nishul@me.com>
2015-02-02 16:29:04 +0000

    Completed 9.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,18 @@ class UsersController < ApplicationController
 
   def show
   		@user = User.find(params[:id])
-  	end
+  end
 
 	def new
   	@user = User.new
-  	end
+  end
 
-  	def create
-  		@user = User.new(user_params)
-  		if @user.save
-  			log_in @user
-  			flash[:success] = "Welcome to the Sample App!"
-  			redirect_to @user
+  def create
+		@user = User.new(user_params)
+  	if @user.save
+  		log_in @user
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
 		else
 			render 'new'
 		end
